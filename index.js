27af505d5973958c80dffd286590597399c1618b
princethakur19 <princethakur7778@gmail.com>
2026-04-27 09:50:40 +0530

    easy method to to use async and await

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-async function greet(){
-    return "Prince"
+// async function greet(){
+//     return "Prince"
+// }
+
+// const response = greet();
+// // console.log(response);
+// response.then((data)=> console.log(data))/
+
+async function github(){
+    const response = await fetch("https://api.github.com/users");
+    const data = await response.json();
+    console.log(data)
 }
 
-const response = greet();
-// console.log(response);
-response.then((data)=> console.log(data))
+github()
+console.log("Hello how are you?")
